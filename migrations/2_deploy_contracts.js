dcef8dd066c9d7e6e520e63bfeac728d6fdb093a
Evan Alioto <EvanJAlioto@gmail.com>
2021-03-30 22:48:24 -0700

    smart contracts deployed to test server, seeded and ready for front end implementation

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,13 @@
 const Token = artifacts.require("Token");
-module.exports = function (deployer) {
-  deployer.deploy(Token);
+const Exchange = artifacts.require('Exchange');
+
+module.exports =  async function (deployer) {
+  const accounts = await web3.eth.getAccounts()
+  
+  await deployer.deploy(Token);
+
+  const feeAccount = accounts[0];
+  const feePercent = 10;
+
+  await deployer.deploy(Exchange, feeAccount, feePercent);
 };
--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -11,6 +11,7 @@ contract Exchange {
     mapping(uint256 => _Order) public orders; // Store the order
     mapping(uint256 => bool) public orderCancelled;
     uint256 public orderCount;
+    mapping(uint256 => bool) public orderFilled;
 
     // events
     event Deposit(address token, address user, uint256 amount, uint256 balance);
@@ -36,6 +37,16 @@ contract Exchange {
         uint256 amountGet,
         address tokenGive,
         uint256 amountGive,
+        uint256 timestamp
+    );
+    event Trade(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        address userFill,
         uint256 timestamp
     );
 
@@ -144,6 +155,57 @@ contract Exchange {
             now
         );
     }
+
+    function fillOrder(uint256 _id) public {
+        require(_id > 0 && _id <= orderCount);
+        require(!orderFilled[_id]);
+        require(!orderCancelled[_id]);
+        _Order storage _order = orders[_id];
+        _trade(
+            _order.id,
+            _order.user,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive
+        );
+        orderFilled[_order.id] = true;
+    }
+
+    function _trade(
+        uint256 _orderId,
+        address _user,
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive
+    ) internal {
+        // Fee paid by the user that fills the order, a.k.a. msg.sender.
+        uint256 _feeAmount = _amountGet.mul(feePercent).div(100);
+
+        tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender].sub(
+            _amountGet.add(_feeAmount)
+        );
+        tokens[_tokenGet][_user] = tokens[_tokenGet][_user].add(_amountGet);
+        tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount].add(
+            _feeAmount
+        );
+        tokens[_tokenGive][_user] = tokens[_tokenGive][_user].sub(_amountGive);
+        tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(
+            _amountGive
+        );
+
+        emit Trade(
+            _orderId,
+            _user,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            msg.sender,
+            now
+        );
+    }
 }
 /*
 TODO:
@@ -155,6 +217,6 @@ TODO:
     [x] Check balances
     [x] Make Order
     [x] Cancel Order
-    [] Fill Order
-    [] Charge fees
+    [x] Fill Order
+    [x] Charge fees
 */
