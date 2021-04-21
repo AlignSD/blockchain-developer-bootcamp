d2747117a0994a26045236dc58fbf92f8f9a3fb3
Evan Alioto <EvanJAlioto@gmail.com>
2021-04-21 00:12:24 -0700

    minor refractor

--- src/components/content.js
+++ src/components/content.js
@@ -23,33 +23,33 @@ class Content extends Component {
   render() {
     return (
       <div className="content">
-          <div className="vertical-split">
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
+        <div className="vertical-split">
+          <div className="card bg-dark text-white">
+            <div className="card-header">
+              Card Title
             </div>
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
+            <div className="card-body">
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="/#" className="card-link">Card link</a>
             </div>
           </div>
-          <OrderBook />
-          <div className="vertical-split">
-            <PriceChart />
-            <MyTransactions />
+          <div className="card bg-dark text-white">
+            <div className="card-header">
+              Card Title
+            </div>
+            <div className="card-body">
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="/#" className="card-link">Card link</a>
+            </div>
           </div>
-          <Trades/>
         </div>
+          <OrderBook />
+        <div className="vertical-split">
+          <PriceChart />
+          <MyTransactions />
+        </div>
+        <Trades/>
+      </div>
     )
   }
 }
